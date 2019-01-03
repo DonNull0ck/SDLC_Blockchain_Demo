ae7651664cc53e2e39f80b131c4175794acef92c
DonNull0ck <DonNull0ck@users.noreply.github.com>
2019-01-03 16:01:19 -0500

    Add files via upload

---
+++ patient.sol
@@ -0,0 +1,26 @@
+//import solidity
+
+pragma solidity ^0.4.0
+
+contract Patient { //could also use a struct probably
+
+	string Name;
+	uint Patient_ID;
+	string SSN;
+	string DateOfBirth;
+	uint Age;
+	string FullAddress;
+	string EmailAddress;
+	string Phone;
+	string Employer;
+	string InsurancePlan;
+	uint InsuranceGroupNumber;
+	bool Dependents;
+	string DependentName;
+	uint DependentID;
+	string Symptoms;
+	string PrimaryCarePhysician;
+	string MedicalHistory;
+	string Allergies;
+	
+}
